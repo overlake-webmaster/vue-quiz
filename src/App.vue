fdedf187c9e2ff757a7cdd06069eba5a240fb490
Sam Yeager <syeager@overlake.org>
2019-04-16 11:01:43 -0700

    initial commit; finished tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header
+      :correctAnswers="correctAnswers"
+      :totalAnswers="totalAnswers"/>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox
+          v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
+  },
+  data() {
+    return {
+        questions: [],
+        index: 0,
+        correctAnswers: 0,
+        totalAnswers: 0
+    }
+  },
+  methods: {
+    handleNext(isCorrect) {
+      increment(isCorrect);
+      next();
+    },
+    increment(isCorrect) {
+        if (isCorrect) {
+            this.correctAnswers++;
+        }
+        this.totalAnswers++;
+    },
+    next() {
+      this.index++;
+    },
+  },
+  mounted:  function() {
+    fetch('https://opentdb.com/api.php?amount=10&category=27&type=multiple', {
+        method: 'get'
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((jsonData) => {
+        this.questions = jsonData.results
+    })
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+         <b-nav tabs>
+            <b-nav-item disabled><strong>Quiz App</strong></b-nav-item>
+            <b-nav-item disabled>Counter {{ correctAnswers }}/ {{ totalAnswers }}</b-nav-item>
+        </b-nav>
+        <div v-show="totalAnswers === 10">
+            Congrats! You answered them all
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: {
+        correctAnswers: Number,
+        totalAnswers: Number
+    },
+}
+</script>
